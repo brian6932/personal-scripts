4510cf3b28ffb54e8cc4196070dac1b0f6df93f2
Brian <18603393+brian6932@users.noreply.github.com>
2022-03-01 22:25:18 -0500

    Fixed broken args

--- CLGrab.ps1
+++ CLGrab.ps1
@@ -1,13 +1,13 @@
 #Requires -Version 7.2.00000.000
 $channel = $args[0].ToLower()
-$fossaMode = $args[1] -or $args[2]
-$pasteMode = $args[1] -or $args[2]
+$fossaMode = $args[1] -eq '-f' -or $args[2] -eq '-f'
+$pasteMode = $args[1] -eq '-p' -or $args[2] -eq '-p'
 if ($channel -ne '-p' -and $channel -ne '-f') {
     if ($channel) {
         $request = pf -f https://tmi.twitch.tv/group/user/$channel/chatters | jq -r '.chatters.broadcaster, .chatters.vips, .chatters.moderators, .chatters.viewers | .[]' | rg -U '.\n' -r ' '
         $charCount = $request | measure -Line -Character | Select -ExpandProperty Characters
-        $splitLength = $fossaMode -eq '-f' ? 375 : 475
-        $pasteMode -eq '-p' ?
+        $splitLength = $fossaMode ? 375 : 475
+        $pasteMode ?
         $charCount -ge 500 ? ($request | rg --pcre2 ".{$splitLength}\K\s" -r `n | pf | scb -PassThru) : ($request | pf | scb -PassThru):
         $charCount -ge 500 ? ($request | rg --pcre2 ".{$splitLength}\K\s" -r `n | scb -PassThru) : ($request | scb -PassThru)
     }
