794ba69dec534bc3dca380af6d0f9c0c00ccd2f0
Brian <18603393+brian6932@users.noreply.github.com>
2022-04-04 19:57:35 -0400

    Aliases :)

--- HShield.ps1
+++ HShield.ps1
@@ -1,3 +1,3 @@
-Start-Service 'hshld_*'
-Start-Process -Wait "C:\Program Files (x86)\Hotspot Shield\*\bin\hsscp.exe"
-Stop-Service 'hshld_*'
+spps 'hshld_*'
+spps -Wait "C:\Program Files (x86)\Hotspot Shield\*\bin\hsscp.exe"
+spsv 'hshld_*'
