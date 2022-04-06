d6abc10c8bb3a0dcedf2a1e70dc4c206855c321a
Brian <18603393+brian6932@users.noreply.github.com>
2022-04-06 00:01:43 -0400

    xD this is simpler

--- CLGrab.ps1
+++ CLGrab.ps1
@@ -5,7 +5,7 @@ $pasteMode = $args[1] -eq '-p' -or $args[2] -eq '-p'
 if ($channel -ne '-p' -and $channel -ne '-f') {
     if ($channel) {
         $request = pf -f https://tmi.twitch.tv/group/user/$channel/chatters | jq -r '.chatters.broadcaster, .chatters.vips, .chatters.moderators, .chatters.viewers | .[]' | rg -U '.\n' -r ' '
-        $charCount = $request | measure -Line -Character | Select -ExpandProperty Characters
+        $charCount = $request.length
         $splitLength = $fossaMode ? 375 : 475
         $pasteMode ?
         $charCount -ge 500 ? ($request | rg --pcre2 ".{$splitLength}\K\s" -r `n | pf | scb -PassThru) : ($request | pf | scb -PassThru):
