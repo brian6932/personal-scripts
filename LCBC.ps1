8290ba72bc961139630331ea7edc1d5dcf5c0746
Brian <18603393+brian6932@users.noreply.github.com>
2022-08-18 08:38:00 -0400

    Optimized LCBC further, and made LCUC

--- LCBC.ps1
+++ LCBC.ps1
@@ -2,9 +2,13 @@
 #Requires -Version 7.0.00000.000
 $logs = $args[0].ToLower()
 $request = curl -sSL https://$logs/channels | jq -r '.channels[].userID'
-$num = (curl -sSL https://$logs/channels | jq -r '.channels[].userID').length
+$num = $request.length
 Write-Host "$num users found on instance" -f red
+
 if ($num -gt 50) {
+    $start = 0
+    $range = 0
+    $request = for ($request -lt $num; $xd = $request[($start + $range)..($range += 49 + $start)]; $start++) { $xd -join ',' }
     $request | % -pa { curl -sSL https://api.ivr.fi/v2/twitch/user?id=$_ | jq -r '.[] | select(.banned == true) | .id' }
 }
 else {
---
+++ LCUC.ps1
@@ -0,0 +1,11 @@
+#Logged Channel User Checker
+#Requires -Version 7.0.00000.000
+$logs = $args[0].ToLower()
+$user = curl -fsSL "https://api.ivr.fi/v2/twitch/user/$($args[1].ToLower())" | jq -r .id
+
+if ($user) {
+    $request = curl -sSL https://$logs/channels | jq -r '.channels[].userID'
+    Write-Host "$($request.length) users found on instance" -f red
+    $request | % { if (curl -fsL "https://$logs/list?channelid=$_&userid=$user") { $_ } }
+}
+else { Write-Host "User not found" -f red }
