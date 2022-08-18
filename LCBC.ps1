4ee026e35cd452ec1be8713536424dee447eee96
Brian <18603393+brian6932@users.noreply.github.com>
2022-08-18 15:45:06 -0400

    Usability improvements

--- LCBC.ps1
+++ LCBC.ps1
@@ -1,17 +1,19 @@
 #Logged Channel Ban Checker
 #Requires -Version 7.0.00000.000
-$logs = $args[0].ToLower()
-$request = curl -sSL https://$logs/channels | jq -r '.channels[].userID'
+$request = curl -fsL "https://$($args[0].ToLower())/channels" | jq -r '.channels[].userID'
 $num = $request.length
 Write-Host "$num users found on instance" -f red
 
-if ($num -gt 50) {
-    $start = 0
-    $range = 0
-    $request = for ($request -lt $num; $xd = $request[($start + $range)..($range += 49 + $start)]; $start++) { $xd -join ',' }
-    $request | % -pa { curl -sSL https://api.ivr.fi/v2/twitch/user?id=$_ | jq -r '.[] | select(.banned == true) | .id' }
+if ($request) {
+    if ($num -gt 50) {
+        $start = 0
+        $range = 0
+        $request = for ($request -lt $num; $xd = $request[($start + $range)..($range += 49 + $start)]; $start++) { $xd -join ',' }
+        $request | % -pa { curl -sSL https://api.ivr.fi/v2/twitch/user?id=$_ | jq -r '.[] | select(.banned == true) | .id' }
+    }
+    else {
+        $in = $request -join ','
+        curl -sSL https://api.ivr.fi/v2/twitch/user?id=$in | jq -r '.[] | select(.banned == true) | .id'
+    }
 }
-else {
-    $in = $request -join ','
-    curl -sSL https://api.ivr.fi/v2/twitch/user?id=$in | jq -r '.[] | select(.banned == true) | .id'
-}
+else { Write-Host "Instance not found" -f red }
--- LCUC.ps1
+++ LCUC.ps1
@@ -4,8 +4,11 @@ $logs = $args[0].ToLower()
 $user = curl -fsL "https://api.ivr.fi/v2/twitch/user/$($args[1].ToLower())" | jq -r .id
 
 if ($user) {
-    $request = curl -sSL https://$logs/channels | jq -r '.channels[].userID'
-    Write-Host "$($request.length) users found on instance" -f red
-    $request | % { if (curl -fsL "https://$logs/list?channelid=$_&userid=$user") { $_ } }
+    $request = curl -fsL https://$logs/channels | jq -r '.channels[].userID'
+    if ($request) {
+        Write-Host "$($request.length) users found on instance" -f red
+        $request | % { if (curl -fsL "https://$logs/list?channelid=$_&userid=$user") { $_ } }
+    }
+    else { Write-Host "Instance not found" -f red }
 }
 else { Write-Host "User not found" -f red }
