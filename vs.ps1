eef49a59c7ce0f283269c101baf5449f31ac26b9
Brian <18603393+brian6932@users.noreply.github.com>
2022-12-07 14:19:40 -0500

    vs: Use wildcard for year

--- vs.ps1
+++ vs.ps1
@@ -1,3 +1,3 @@
 # Imports Visual Studio Build Tools env vars on demand
-Import-Module 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'
+Import-Module 'C:\Program Files (x86)\Microsoft Visual Studio\*\BuildTools\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'
 Enter-VsDevShell 08d8510f
