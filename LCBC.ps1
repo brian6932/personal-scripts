6506ada2bb02163f931b36906f95296e054d2494
Brian <18603393+brian6932@users.noreply.github.com>
2023-01-11 10:00:23 -0500

    LCBC: Remove dependencies

--- LCBC.ps1
+++ LCBC.ps1
@@ -1,19 +1,23 @@
 # Logged Channel Ban Checker
 #Requires -Version 7.0.00000.000
-$request = curl -fsL "https://$($args[0].ToLower())/channels" | jq -r '.channels[].userID'
-$num = $request.length
-Write-Host "$num users found on instance" -f red
+try {
+    $request = (Invoke-RestMethod "https://$($args[0].ToLower())/channels").channels.userID
+    $num = $request.length
+    Write-Host "$num users found on instance" -f red
 
-if ($request) {
     if ($num -gt 50) {
         $start = 0
         $range = 0
-        $request = for ($request -lt $num; $xd = $request[($start + $range)..($range += 49 + $start)]; $start++) { $xd -join ',' }
-        $request | % -pa { curl -sSL https://api.ivr.fi/v2/twitch/user?id=$_ | jq -r '.[] | select(.banned == true) | .id' }
+        $request = for ($request -lt $num; $xd = $request[($start + $range)..($range += 49 + $start)]; $start++) {
+            $xd -join ','
+        }
+        ($request | ForEach-Object -Parallel { Invoke-RestMethod https://api.ivr.fi/v2/twitch/user?id=$_ } | Where-Object { $_.banned -eq $true }).id
     }
     else {
         $in = $request -join ','
-        curl -sSL https://api.ivr.fi/v2/twitch/user?id=$in | jq -r '.[] | select(.banned == true) | .id'
+        (Invoke-RestMethod https://api.ivr.fi/v2/twitch/user?id=$in | Where-Object { $_.banned -eq $true }).id
     }
 }
-else { Write-Host "Instance not found" -f red }
+catch {
+    Write-Host "Instance not found" -f red
+}
