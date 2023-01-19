63ade2c66031db165bf788ee73bfcc23397bd97b
Brian <18603393+brian6932@users.noreply.github.com>
2023-01-18 19:47:58 -0500

    LCUC: Removed curl dep

--- LCUC.ps1
+++ LCUC.ps1
@@ -1,14 +1,24 @@
 # Logged Channel User Checker
 #Requires -Version 7.0.00000.000
-$logs = $args[0].ToLower()
-$user = curl -fsL "https://api.ivr.fi/v2/twitch/user?login=$($args[1].ToLower())" | jq -r .[].id
-
-if ($user) {
-    $request = curl -fsL https://$logs/channels | jq -r '.channels[].userID'
+try {
+    $logs = $args[0].ToLower()
+    $user = (Invoke-RestMethod "https://api.ivr.fi/v2/twitch/user?login=$($args[1].ToLower())").id
+    $request = (Invoke-RestMethod https://$logs/channels).channels.userID
     if ($request) {
         Write-Host "$($request.length) users found on instance" -f red
-        $request | % { if (curl -fsL "https://$logs/list?channelid=$_&userid=$user") { $_ } }
+        $request | ForEach-Object {
+            try {
+                if (Invoke-RestMethod "https://$logs/list?channelid=$_&userid=$user") {
+                    $_
+                }
+            }
+            catch {}
+        }
     }
-    else { Write-Host "Instance not found" -f red }
+    else {
+        Write-Host "Instance not found" -f red
+    }
 }
-else { Write-Host "User not found" -f red }
+catch {
+    Write-Host "User not found" -f red
+}
