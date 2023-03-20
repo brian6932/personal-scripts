e1871c3c05d538821e6fef0e43245221d997a5b9
Brian <18603393+brian6932@users.noreply.github.com>
2023-03-19 22:16:03 -0400

    ucollect: Don't alias

--- ucollect.ps1
+++ ucollect.ps1
@@ -13,4 +13,4 @@ foreach ($arg in $args) {
 $logs = (Invoke-WebRequest https://$instance/list?channelid=$uid).Content | jq -r '.availableLogs[] | .year+"/"+.month+"/"+.day' | Sort-Object -Unique | ForEach-Object -Parallel {
     Invoke-RestMethod "https://$using:instance/channelid/$using:uid/$_`?raw"
 } | Select-String '^@\S+ :([a-z0-9]\w{0,24})!\1@\1\.tmi\.twitch\.tv'
-[Collections.Generic.HashSet[string]]::new([string[]]($logs.matches.groups | ? { $_.Name -eq 1 }).Value) > uniqueChanChatters
+[Collections.Generic.HashSet[string]]::new([string[]]($logs.matches.groups | Where-Object { $_.Name -eq 1 }).Value) > uniqueChanChatters
