f35cd1500a029a77634ab4fb3a437175f7f0e3ab
Brian <18603393+brian6932@users.noreply.github.com>
2023-07-06 10:54:17 -0400

    ucollect: Rewrite

--- ucollect.ps1
+++ ucollect.ps1
@@ -1,16 +1,20 @@
 foreach ($arg in $args) {
-    if ($arg -match "\A\d+\Z") {
+    if ($arg -match "\A\d+\Z" -and !$uid) {
         $uid = $arg
-        break
+        continue
     }
-}
-foreach ($arg in $args) {
-    if ($arg -match '\Ai:\S+\Z') {
+    if ($arg -match '\Ai:\S+\Z' -and !$instance) {
         $instance = $arg.Substring(2)
+    }
+    if ($uid -and $instance) {
         break
     }
 }
-$logs = (Invoke-WebRequest https://$instance/list?channelid=$uid).Content | jq -r '.availableLogs[] | .year+"/"+.month+"/"+.day' | Sort-Object -Unique | ForEach-Object -Parallel {
-    Invoke-RestMethod "https://$using:instance/channelid/$using:uid/$_`?raw"
-} | Select-String '^@\S+ :([a-z0-9]\w{0,24})!\1@\1\.tmi\.twitch\.tv'
-[Collections.Generic.HashSet[string]]::new([string[]]($logs.matches.groups | Where-Object { $_.Name -eq 1 }).Value) > uniqueChanChatters
+
+$logs = [System.Text.StringBuilder]::new()
+$logs.Capacity = 500000000
+(Invoke-WebRequest https://$instance/list?channelid=$uid).Content | jq -r '.availableLogs[] | .year+"/"+.month+"/"+.day' | ForEach-Object -Parallel {
+    ($using:logs).Append((Invoke-RestMethod "https://$using:instance/channelid/$using:uid/$_`?raw"))
+} -ThrottleLimit (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
+
+[Collections.Generic.HashSet[string]]($logs.ToString() | rg -UPo '^@\S+ :([a-z\d]\w{0,24})!\1@\1\.tmi\.twitch\.tv' -r '$1')
