83de6c56002fc19675047fa9d192b3b145308a73
Brian <18603393+brian6932@users.noreply.github.com>
2023-08-26 15:37:10 -0400

    ucollect: Make cross platform

--- ucollect.ps1
+++ ucollect.ps1
@@ -1,10 +1,9 @@
 foreach ($arg in $args) {
-    if ($arg -match "\A\d+\Z" -and !$uid) {
-        $uid = $arg
-        continue
+    $uid ??= if ($arg -match '\A\d+\Z') {
+        $arg
     }
-    if ($arg -match '\Ai:\S+\Z' -and !$instance) {
-        $instance = $arg.Substring(2)
+    $instance ??= if ($arg -match '\Ai:\S+\Z') {
+        $arg.Substring(2)
     }
     if ($uid -and $instance) {
         break
@@ -15,6 +14,6 @@ $logs = [System.Text.StringBuilder]::new()
 $logs.Capacity = 500000000
 (Invoke-WebRequest https://$instance/list?channelid=$uid).Content | jq -r '.availableLogs[] | .year+"/"+.month+"/"+.day' | ForEach-Object -Parallel {
     ($using:logs).Append((Invoke-RestMethod "https://$using:instance/channelid/$using:uid/$_`?raw"))
-} -ThrottleLimit (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
+} -ThrottleLimit [Environment]::ProcessorCount
 
-[Collections.Generic.HashSet[string]]($logs.ToString() | rg -UPo '^@\S+ :([a-z\d]\w{0,24})!\1@\1\.tmi\.twitch\.tv' -r '$1')
+[Collections.Generic.HashSet[string]]($logs.ToString() | rg -UPo '^@\S+ :([a-z\d]\w{0,24})!\1@\1\Q.tmi.twitch.tv\E' -r `$1)
