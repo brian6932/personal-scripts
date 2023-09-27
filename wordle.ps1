de5376d52e02c8fbc61b6305f3eaa429ee46592e
Brian <18603393+brian6932@users.noreply.github.com>
2023-09-27 07:42:14 -0400

    I'm so stupid

--- wordle.ps1
+++ wordle.ps1
@@ -1,1 +1,1 @@
-wordle custom (Invoke-RestMethod "https://www.nytimes.com/svc/wordle/v2/$((Get-Date).ToString('yyyy-MM-dd')).json").solution
+wordle.exe custom (Invoke-RestMethod "https://www.nytimes.com/svc/wordle/v2/$((Get-Date).ToString('yyyy-MM-dd')).json").solution
