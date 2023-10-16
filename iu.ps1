9e9157dfd98b87f07d030c42f04440bcb458faa9
Brian <18603393+brian6932@users.noreply.github.com>
2023-10-16 18:17:43 -0400

    add iuc

--- iu.ps1
+++ iu.ps1
@@ -1,2 +1,1 @@
-# curl way: curl -X POST https://i.alienpls.org/upload -F "file=@$($args[0])"
 Invoke-RestMethod https://i.alienpls.org/upload -Method Post -Form @{ file = Get-Item $args[0] } | Set-Clipboard -PassThru
---
+++ iuc.ps1
@@ -0,0 +1,1 @@
+curl.exe -X POST https://i.alienpls.org/upload -F "file=@$($args[0])"
