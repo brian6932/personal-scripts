46afee81f9d41ef1372f0720bcec33e04eaec7c7
Brian <18603393+brian6932@users.noreply.github.com>
2023-10-19 21:38:53 -0400

    Pepega

--- ucollect.ps1
+++ ucollect.ps1
@@ -14,6 +14,6 @@ $logs = [System.Text.StringBuilder]::new()
 $logs.Capacity = 500000000
 (Invoke-WebRequest https://$instance/list?channelid=$uid).Content | jq -r '.availableLogs[] | .year+"/"+.month+"/"+.day' | ForEach-Object -Parallel {
     ($using:logs).Append((Invoke-RestMethod "https://$using:instance/channelid/$using:uid/$_`?raw"))
-} -ThrottleLimit [Environment]::ProcessorCount
+} -ThrottleLimit ([Environment]::ProcessorCount)
 
 [Collections.Generic.HashSet[string]]($logs.ToString() | rg -UPo '^@\S+ :([a-z\d]\w{0,24})!\1@\1\Q.tmi.twitch.tv\E' -r `$1)
