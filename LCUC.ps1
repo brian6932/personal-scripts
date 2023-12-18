9d4b4746ef00e46a9ec027704b4af57fc6add925
Brian <18603393+brian6932@users.noreply.github.com>
2023-12-17 23:32:47 -0500

    lcuc: Optimize

--- LCUC.ps1
+++ LCUC.ps1
@@ -1,24 +1,33 @@
 # Logged Channel User Checker
 #Requires -Version 7.0.00000.000
+
+if ($args.length -lt 2) {
+    throw '2 arguments required'
+}
+
 try {
     $logs = $args[0].ToLower()
-    $user = (Invoke-RestMethod "https://api.ivr.fi/v2/twitch/user?login=$($args[1].ToLower())").id
+    $user = try {
+        [UInt16]$args[1]
+    }
+    catch {
+        $args[1] = ([string]$args[1]).Substring($args[1][1] -eq '@')
+        (Invoke-RestMethod https://gql.twitch.tv/gql -Method Post -Body '{"query":"{user(login:\"'+$args[1]+'\"lookupType:ALL){id}}"}' -Headers @{'client-id'='ue6666qo983tsx6so1t0vnawi233wa'}).id
+    }
     $request = (Invoke-RestMethod https://$logs/channels).channels.userID
-    if ($request) {
-        Write-Host "$($request.length) users found on instance" -f red
-        $request | ForEach-Object {
-            try {
-                if (Invoke-RestMethod "https://$logs/list?channelid=$_&userid=$user") {
-                    $_
-                }
-            }
-            catch {}
+    if (!$request) {
+        Write-Host 'Instance not found' -ForegroundColor Red
+        exit 127
+    }
+    Write-Host "$($request.length) users found on instance" -ForegroundColor Red
+    $request | ForEach-Object -Parallel {
+        try {
+            $null = Invoke-WebRequest -Method Head "https://$using:logs/list?channelid=$_&userid=$using:user"
+            $_
         }
-    }
-    else {
-        Write-Host "Instance not found" -f red
-    }
+        catch {}
+    } -ThrottleLimit ([Environment]::ProcessorCount)
 }
 catch {
-    Write-Host "User not found" -f red
+    Write-Host 'User not found' -ForegroundColor Red
 }
