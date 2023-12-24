8e94e8b5505b2a0d4428f1eea166a54a4c5a8af0
Brian <18603393+brian6932@users.noreply.github.com>
2023-12-24 01:45:55 -0500

    rup: Make not shit

--- rup.ps1
+++ rup.ps1
@@ -1,2 +1,2 @@
 # Checks for updates for all choco packages with Rocolatey, then updates all the aforementioned packages
-choco upgrade (((roco outdated -r) | ForEach-Object { $_.Split('|')[0] }))
+choco upgrade (roco outdated -r | ForEach-Object { $_.Substring(0, $_.IndexOf('|')) })
