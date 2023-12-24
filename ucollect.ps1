a48037f940626252825262954f9a671b396062a9
Brian <18603393+brian6932@users.noreply.github.com>
2023-12-24 03:24:46 -0500

    ucollect: Allocate on instantiation

--- ucollect.ps1
+++ ucollect.ps1
@@ -10,8 +10,7 @@ foreach ($arg in $args) {
     }
 }
 
-$logs = [System.Text.StringBuilder]::new()
-$logs.Capacity = 500000000
+$logs = [Text.StringBuilder]::new($null, 500000000)
 (Invoke-WebRequest https://$instance/list?channelid=$uid).Content | jq -r '.availableLogs[] | .year+"/"+.month+"/"+.day' | ForEach-Object -Parallel {
     ($using:logs).Append((Invoke-RestMethod "https://$using:instance/channelid/$using:uid/$_`?raw"))
 } -ThrottleLimit ([Environment]::ProcessorCount)
