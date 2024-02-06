0805abbedbd72008e90a6466fb3c385e93a694ad
Brian <18603393+brian6932@users.noreply.github.com>
2024-02-06 02:34:35 -0500

    vs: Automate instance fetch

--- vs.ps1
+++ vs.ps1
@@ -1,3 +1,3 @@
 # Imports Visual Studio Build Tools env vars on demand
-Import-Module 'C:\Program Files (x86)\Microsoft Visual Studio\*\BuildTools\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'
-Enter-VsDevShell 08d8510f -DevCmdArguments '-arch=x64 -no_logo'
+Import-Module 'C:/Program Files (x86)/Microsoft Visual Studio/*/BuildTools/Common7/Tools/Microsoft.VisualStudio.DevShell.dll'
+Enter-VsDevShell (Get-ChildItem C:/ProgramData/Microsoft/VisualStudio/Packages/_Instances)[0].Name -DevCmdArguments '-arch=x64 -no_logo'
