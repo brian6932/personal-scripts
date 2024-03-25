d48261fd6482b60128bb456ae1c583cdfc19a971
Brian <18603393+brian6932@users.noreply.github.com>
2024-03-24 21:07:14 -0400

    vs: Sort instances

--- vs.ps1
+++ vs.ps1
@@ -1,3 +1,4 @@
 # Imports Visual Studio Build Tools env vars on demand
+# -Descending used instead of bottom to keep compatibility with PowerShell 5
 Import-Module 'C:/Program Files (x86)/Microsoft Visual Studio/*/BuildTools/Common7/Tools/Microsoft.VisualStudio.DevShell.dll'
-Enter-VsDevShell (Get-ChildItem C:/ProgramData/Microsoft/VisualStudio/Packages/_Instances)[0].Name -DevCmdArguments '-arch=x64 -no_logo'
+Enter-VsDevShell (Get-ChildItem C:/ProgramData/Microsoft/VisualStudio/Packages/_Instances | Sort-Object LastWriteTime -Descending)[0].Name -DevCmdArguments '-arch=x64 -no_logo'
