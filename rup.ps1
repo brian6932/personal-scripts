626fa16d53a508a90488bbe67410164731dc2c88
Brian <18603393+brian6932@users.noreply.github.com>
2024-09-09 13:25:30 -0400

    rup: Replace `choco-mode` due to it not working as expected with `pre`

--- rup.ps1
+++ rup.ps1
@@ -1,2 +1,2 @@
 # Checks for updates for all choco packages with Rocolatey, then updates all the aforementioned packages
-choco upgrade (roco outdated -rp --choco-mode | ForEach-Object { $_.Substring(0, $_.IndexOf('|')) })
+choco upgrade (roco outdated -rp --ignore-pinned --ignore-unfound | ForEach-Object { $_.Substring(0, $_.IndexOf('|')) })
