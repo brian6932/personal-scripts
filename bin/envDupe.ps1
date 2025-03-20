d7ea45b54b808c9ae6f05e585baea086b983c4f6
Brian <18603393+brian6932@users.noreply.github.com>
2025-03-20 07:23:25 -0400

    envDupe: make crossplaform

--- bin/envDupe.ps1
+++ bin/envDupe.ps1
@@ -1,3 +1,4 @@
 # Removes duplicates in $env:Path
-[Environment]::SetEnvironmentVariable('Path', ([Environment]::GetEnvironmentVariable('Path', 'Machine') -split ';' | Sort-Object -Unique) -join ';', 'Machine')
-[Environment]::SetEnvironmentVariable('Path', ([Environment]::GetEnvironmentVariable('Path', 'User') -split ';' | Sort-Object -Unique) -join ';', 'User')
+'Machine', 'User' | ForEach-Object {
+    [Environment]::SetEnvironmentVariable('Path', ([Environment]::GetEnvironmentVariable('Path', $_) -split [IO.Path]::PathSeparator | Sort-Object -Unique) -join [IO.Path]::PathSeparator, $_)
+}
