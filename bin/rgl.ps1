f6bdab1830cd76323a12d22f09dddb339603d4e2
Brian <18603393+brian6932@users.noreply.github.com>
2025-04-04 18:52:59 -0400

    rgl: Fix args

--- bin/rgl.ps1
+++ bin/rgl.ps1
@@ -1,1 +1,1 @@
-(& (Get-Command -CommandType Application rg)[0].Name $args[0] --json | ConvertFrom-Json).data | ForEach-Object { if ($_.line_number) { $_.path.text + ':' + $_.line_number } }
+(& (Get-Command -CommandType Application rg)[0].Name --json $args | ConvertFrom-Json).data | ForEach-Object { if ($_.line_number) { $_.path.text + ':' + $_.line_number } }
