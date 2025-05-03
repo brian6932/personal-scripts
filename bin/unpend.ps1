6512f5030a4c048a8858f6f659c168182cf882e2
Brian <18603393+brian6932@users.noreply.github.com>
2025-05-02 20:56:53 -0400

    unpend: Fix script

--- bin/unpend.ps1
+++ bin/unpend.ps1
@@ -1,27 +1,29 @@
-Get-ItemProperty (
-	'HKLM:/SYSTEM/CurrentControlSet/Control/Session Manager/PendingFileRenameOperations',
-	'HKLM:/SYSTEM/CurrentControlSet/Control/Session Manager/PendingFileRenameOperations2',
-	'HKLM:/SOFTWARE/Microsoft/Windows/CurrentVersion/WindowsUpdate/Auto Update/RebootRequired',
-	'HKLM:/SOFTWARE/Microsoft/Windows/CurrentVersion/WindowsUpdate/Auto Update/PostRebootReporting',
-	'HKLM:/SOFTWARE/Microsoft/Windows/CurrentVersion/RunOnce/DVDRebootSignal',
-	'HKLM:/Software/Microsoft/Windows/CurrentVersion/Component Based Servicing/RebootPending',
-	'HKLM:/Software/Microsoft/Windows/CurrentVersion/Component Based Servicing/RebootInProgress',
-	'HKLM:/Software/Microsoft/Windows/CurrentVersion/Component Based Servicing/PackagesPending',
-	'HKLM:/SOFTWARE/Microsoft/ServerManager/CurrentRebootAttempts',
-	'HKLM:/SYSTEM/CurrentControlSet/Services/Netlogon/JoinDomain',
-	'HKLM:/SYSTEM/CurrentControlSet/Services/Netlogon/AvoidSpnSet'
-) -ErrorAction Ignore | Remove-Item
+@(
+	@('HKLM:/SYSTEM/CurrentControlSet/Control/Session Manager', 'PendingFileRenameOperations')
+	@('HKLM:/SOFTWARE/Microsoft/Windows/CurrentVersion/WindowsUpdate/Auto Update', 'RebootRequired')
+	@('HKLM:/SYSTEM/CurrentControlSet/Control/Session Manager', 'PendingFileRenameOperations2')
+	@('HKLM:/SOFTWARE/Microsoft/Windows/CurrentVersion/WindowsUpdate/Auto Update', 'PostRebootReporting')
+	@('HKLM:/SOFTWARE/Microsoft/Windows/CurrentVersion/RunOnce', 'DVDRebootSignal')
+	@('HKLM:/Software/Microsoft/Windows/CurrentVersion/Component Based Servicing', 'RebootPending')
+	@('HKLM:/Software/Microsoft/Windows/CurrentVersion/Component Based Servicing', 'RebootInProgress')
+	@('HKLM:/Software/Microsoft/Windows/CurrentVersion/Component Based Servicing', 'PackagesPending')
+	@('HKLM:/SOFTWARE/Microsoft/ServerManager', 'CurrentRebootAttempts')
+	@('HKLM:/SYSTEM/CurrentControlSet/Services/Netlogon', 'JoinDomain')
+	@('HKLM:/SYSTEM/CurrentControlSet/Services/Netlogon', 'AvoidSpnSet')
+) | ForEach-Object { Remove-ItemProperty -ErrorAction Ignore @_ }
 
-if (($_ = Get-ItemProperty HKLM:/SOFTWARE/Microsoft/Updates/UpdateExeVolatile -ErrorAction Ignore).UpdateExeVolatile -gt 0) {
-	Remove-Item $_
+$UpdateExeVolatile = @('HKLM:/SOFTWARE/Microsoft/Updates', 'UpdateExeVolatile')
+if ((Get-ItemProperty @UpdateExeVolatile -ErrorAction -Ignore).UpdateExeVolatile -gt 0) {
+	Remove-ItemProperty @UpdateExeVolatile
 }
 
-Get-ChildItem HKLM:/SOFTWARE/Microsoft/Windows/CurrentVersion/WindowsUpdate/Services/Pending | Remove-Item
+Remove-Item 'HKLM:/SOFTWARE/Microsoft/Windows/CurrentVersion/WindowsUpdate/Services/Pending/*'
 
-if (($ActiveComputerName = Get-ItemProperty HKLM:/SYSTEM/CurrentControlSet/Control/ComputerName/ActiveComputerName).ComputerName -ne ($ComputerName = Get-ItemProperty HKLM:/SYSTEM/CurrentControlSet/Control/ComputerName/ComputerName).ComputerName) {
-	$ActiveComputerName, $ComputerName
+$ActiveComputerName = Get-ItemProperty 'HKLM:/SYSTEM/CurrentControlSet/Control/ComputerName/ActiveComputerName' 'ComputerName'
+$ComputerName = Get-ItemProperty 'HKLM:/SYSTEM/CurrentControlSet/Control/ComputerName/ComputerName' 'ComputerName'
+
+if ($ActiveComputerName.ComputerName -ne $ComputerName.ComputerName) {
 	throw "Mismatched
 	$($ActiveComputerName.PSPath) ($($ActiveComputerName.ComputerName))
-	$($ComputerName.PSPath) ($($ComputerName.ComputerName))
-	"
+	$($ComputerName.PSPath) ($($ComputerName.ComputerName))"
 }
