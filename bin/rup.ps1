760faafed7968bda9a44cefcfede87f1e7e333df
Brian <18603393+brian6932@users.noreply.github.com>
2025-06-19 01:42:29 -0400

    rup: Remove script
    There's no need for this anymore, roco upgrade exists now:

--- bin/rup.ps1
+++
@@ -1,2 +0,0 @@
-# Checks for updates for all choco packages with Rocolatey, then updates all the aforementioned packages
-choco upgrade (roco outdated -rp --ignore-pinned --ignore-unfound | ForEach-Object { $_.Substring(0, $_.IndexOf('|')) })
